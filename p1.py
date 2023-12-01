6d316c77bc6b71e51fab0e5cc7458fe7febcfd95
SanchitaBhaktha10 <143251114+SanchitaBhaktha10@users.noreply.github.com>
2023-12-01 17:45:22 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,16 @@
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+# check if 'H' is present in x string
+print('H' in x)
+
+
+
+# check if 'hello' is present in x string
+print('hello' not in x)  
+
+# check if '1' key is present in y
+print(1 in y) 
+
+# check if 'a' key is present in y
+print('a' in y) 
