5891f4c6fa4fda431c864ec113f5965a9488648b
SanchitaBhaktha10 <143251114+SanchitaBhaktha10@users.noreply.github.com>
2023-12-02 10:02:40 +0530

    Add files via upload

---
+++ DivFn.py
@@ -0,0 +1,5 @@
+def div(x,y): # function definition
+    print(x/y)
+a=int(input("Enter a value")) # user input a
+b=int(input("Enter b value")) # user input b
+div(a,b) # function calling 
---
+++ Square.py
@@ -0,0 +1,4 @@
+def square(x): # function definition
+    print(x*x)
+a=int(input("Enter a value")) # user input a
+square(a) # function calling 
