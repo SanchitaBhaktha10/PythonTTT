b5a6b85347e2723e8fedfc7436cd13c558fd573c
SanchitaBhaktha10 <143251114+SanchitaBhaktha10@users.noreply.github.com>
2023-12-29 17:56:59 +0530

    Add files via upload

---
+++ stringpali.py
@@ -0,0 +1,21 @@
+# given string
+given_string = "madam"
+ 
+# an empty string variable to store
+# the given string in reverse
+reverse_string = ""
+ 
+# iterate through the given string
+# and append each element of the given string
+# to the reverse_string variable
+for i in given_string:
+    reverse_string = i + reverse_string  
+ 
+# if given_string matches the reverse_srting exactly
+# the given string is a palindrome
+if(given_string == reverse_string):
+   print("The string", given_string,"is a Palindrome.")
+ 
+# else the given string is not a palindrome   
+else:
+   print("The string",given_string,"is NOT a Palindrome.")
