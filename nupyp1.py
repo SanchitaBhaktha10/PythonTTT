4f555636d515d3d39734aa698a2462063804b0da
SanchitaBhaktha10 <143251114+SanchitaBhaktha10@users.noreply.github.com>
2024-01-11 17:10:30 +0530

    Add files via upload

---
+++ nupyp1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+
+# Create a NumPy array from a Python list
+my_list = [1, 2, 3, 4, 5]
+my_array = np.array(my_list)
+
+# Print the NumPy array
+print("NumPy array:")
+print(my_array)
+
+
+# In[ ]:
+
+
+
+
